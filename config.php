42cd10de01de44b822f74c11e76095a256c464ee
Ramón Cahenzli <rca@psy-q.ch>
2017-02-23 17:43:17 +0100

    Very stupid application.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = [];
+
+//$config['host']     = 'localhost';
+//$config['username'] = 'username';
+//$config['password'] = 'password';
+//$config['database'] = 'database';
+
+$config['host']     = 'localhost';
+$config['username'] = 'yunohost';
+$config['password'] = 'yunohost';
+$config['database'] = 'yunohost_test';
